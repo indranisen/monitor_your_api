c823c1f7ffd1b88a71eb839dbfe9bd1dd3dc5a0c
Indrani <indrani.sen@youview.com>
2016-05-12 16:08:22 +0100

    credentials support added

--- test_lib.py
+++ test_lib.py
@@ -1,1 +1,25 @@
-__author__='Indrani Sen' 
+__author__='Indrani Sen'
+
+import requests
+import xml.etree.ElementTree as ET
+import netrc
+
+api_endpoint = 'https://maps.googleapis.com/maps/api/directions/xml?origin=Toronto&destination=Montreal&key='
+
+def get_api_key(app):
+    try:    
+       info = netrc.netrc()
+       api_id, account, api_key = info.authenticators(app)
+    except IOError:
+       print 'no authentication details loaded'
+       quit()
+    except netrc.NetrcParseError:
+       print 'no authentication details loaded'
+       quit()
+    except TypeError:
+       print 'no authentication details loaded'
+       quit()
+    return api_key
+
+def return_api_endpoint(app):
+    return api_endpoint+get_api_key('google-map-directions')
--- test_runner.py
+++ test_runner.py
@@ -1,1 +1,5 @@
 __author__='Indrani Sen'
+import test_lib as lib
+
+print lib.return_api_endpoint('google-map-directions')
+
