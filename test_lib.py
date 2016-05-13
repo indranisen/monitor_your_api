b0f27c62a4635ef8101a4d69141db1c6f0f86977
Indrani <indrani.sen@youview.com>
2016-05-13 15:33:34 +0100

    indentation fixed

--- test_lib.py
+++ test_lib.py
@@ -7,30 +7,30 @@ api_endpoint = 'https://maps.googleapis.com/maps/api/directions/xml?origin=Toron
 
 
 def get_api_key(app):
-    try:    
-       info = netrc.netrc()
-       api_id, account, api_key = info.authenticators(app)
+    try:
+        info = netrc.netrc()
+        api_id, account, api_key = info.authenticators(app)
     except IOError:
-       print 'no authentication details loaded'
-       quit()
+        print 'no authentication details loaded'
+        quit()
     except netrc.NetrcParseError:
-       print 'no authentication details loaded'
-       quit()
+        print 'no authentication details loaded'
+        quit()
     except TypeError:
-       print 'no authentication details loaded'
-       quit()
+        print 'no authentication details loaded'
+        quit()
     return api_key
 
 
 def return_api_endpoint(app):
-    return api_endpoint+get_api_key('google-map-directions')
+    return api_endpoint+get_api_key(app)
 
 
 def get_request(app):
-    endpoint = api_endpoint+get_api_key('google-map-directions')
+    endpoint = api_endpoint+get_api_key(app)
     actual_response = requests.get(endpoint)
     return _return_response_status_code(actual_response), _return_response_headers(actual_response), \
-           _return_response_content(actual_response), _return_content_type_header_field(actual_response)
+        _return_response_content(actual_response), _return_content_type_header_field(actual_response)
 
 
 def _return_response_headers(response_object):
