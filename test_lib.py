6fb44c98d7b9fb37df6ae267cd0b424e305fad20
Indrani <indrani.sen@youview.com>
2016-05-13 15:25:27 +0100

    content lenght and type support added

--- test_lib.py
+++ test_lib.py
@@ -1,10 +1,10 @@
-__author__='Indrani Sen'
-
 import requests
 import xml.etree.ElementTree as ET
 import netrc
+__author__='Indrani Sen'
 
 api_endpoint = 'https://maps.googleapis.com/maps/api/directions/xml?origin=Toronto&destination=Montreal&key='
+
 
 def get_api_key(app):
     try:    
@@ -21,19 +21,35 @@ def get_api_key(app):
        quit()
     return api_key
 
+
 def return_api_endpoint(app):
     return api_endpoint+get_api_key('google-map-directions')
+
 
 def get_request(app):
     endpoint = api_endpoint+get_api_key('google-map-directions')
     actual_response = requests.get(endpoint)
-    return _return_response_status_code(actual_response), _return_response_headers(actual_response), _return_response_content(actual_response)
+    return _return_response_status_code(actual_response), _return_response_headers(actual_response), \
+           _return_response_content(actual_response), _return_content_type_header_field(actual_response)
+
 
 def _return_response_headers(response_object):
     return response_object.headers
 
+
 def _return_response_status_code(response_object):
     return response_object.status_code
 
+
 def _return_response_content(response_object):
     return response_object.content
+
+
+def _return_content_type_header_field(response_object):
+    return _return_response_headers(response_object)['Content-Type']
+
+
+def _return_content_length_header_field(response_object):
+    return _return_response_headers(response_object)['Content-Length']
+
+
--- test_runner.py
+++ test_runner.py
@@ -1,7 +1,8 @@
-__author__='Indrani Sen'
 import test_lib as lib
+__author__='Indrani Sen'
 
-response_status_code, response_headers, response_content = lib.get_request('google-map-directions')
+response_status_code, response_headers, response_content, content_type = lib.get_request('google-map-directions')
 print response_status_code
 print response_headers
+print content_type
 
